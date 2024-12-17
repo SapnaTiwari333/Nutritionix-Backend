bcc34bbfd68ef2bdf1d912ea2b54620f67f79c73
SapnaTiwari333 <tiwarisapna765@gmail.com>
2024-12-17 13:14:43 +0530

    changes

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -1,21 +1,22 @@
-const asyncHandler =require("express-async-handler");
+const asyncHandler = require("express-async-handler");
 const jwt=require("jsonwebtoken");
 
 const validateToken=asyncHandler(async (req,res,next)=>{
     let token;
-    let authHeader=req.header.Authorization || req.header.authorization;
+    let authHeader=req.headers.Authorization || req.headers.authorization
     if(authHeader && authHeader.startsWith("Bearer")) {
         token=authHeader.split(" ")[1];
-        jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decode)=>{
+        jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
             if(err){
                 res.status(401);
                 throw new Error("User is not authorized");
             }
-            req.user=deoded.user;
+            req.user=decoded.user;
             next();
         });
 
-        if(!token){
+        if(!token)
+            {
             res.status(401);
             throw new Error("User is not authorized or token is missing");
         }
